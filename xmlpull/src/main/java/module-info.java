89eaa42ef312487bec27834decb84a5356bc2647
Petr Široký <siroky.petr@gmail.com>
2017-10-25 13:06:03 +0200

    [#6] add Java 9 module descriptors for xmlpull and xpp3_min

---
+++ xmlpull/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module org.xmlpull.v1 {
+    exports org.xmlpull.v1;
+
+    uses org.xmlpull.v1.XmlPullParser;
+    uses org.xmlpull.v1.XmlSerializer;
+    uses org.xmlpull.v1.XmlPullParserFactory;
+}
